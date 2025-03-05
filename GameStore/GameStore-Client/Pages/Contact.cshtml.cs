59bfe79c162acd2f3f36629cdec30c78ff242a77
ryuker <lifeisagamenotaproduct@gmail.com>
2025-03-05 16:26:23 +0100

    added contact page

---
+++ GameStore/GameStore-Client/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameStore_Client.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
