a40992dbdc8107e7672f1ca8ad712032d0e593da
ryuker <lifeisagamenotaproduct@gmail.com>
2025-03-06 18:01:37 +0100

    retrieved games from api in CatalogModel, added method to convert GameDto to GamesModel, rendered Games on Catalog page

--- GameStore/GameStore-Client/Pages/Catalog.cshtml.cs
+++ GameStore/GameStore-Client/Pages/Catalog.cshtml.cs
@@ -1,19 +1,57 @@
+using GameStore_Client.ApiConnection;
+using GameStore_Client.ApiConnection.Dtos;
+using GameStore_Client.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GameStore_Client.Pages;
 
 public class CatalogModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly IHttpClientFactory _clientFactory;
 
-    public CatalogModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public List<GameModel> Games { get; set; }
+    private readonly GamesClient _client;
+    private readonly ILogger<CatalogModel> _logger;
+
+    public CatalogModel(ILogger<CatalogModel> logger, GamesClient client)
     {
         _logger = logger;
+        _client = client;
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
-      Console.WriteLine("Hello from Catalog Model");   
+      Console.WriteLine("Running Async Method");
+      var res =  await _client.GetGamesAsync();
+      if (res == null)
+      {
+          return NotFound();
+      }
+
+      List<GameModel> games = ToGamesModelList(res);
+      Games = games;
+
+      return Page();
+    }
+
+    static List<GameModel> ToGamesModelList(List<GameDto> games){
+      List<GameModel> gameModelList = [];
+
+      foreach(var game in games){
+        gameModelList.Add(
+          new GameModel{
+            Id = game.Id,
+            Name = game.Name,
+            Genre = game.Genre,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+          }
+        );
+      }
+
+      return gameModelList;
     }
 
 
